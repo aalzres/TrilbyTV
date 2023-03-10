7d2f6c082cee917d47b56e0853119b1e104c49b4
Andrés Felipe <aalzres@gmail.com>
2023-03-10 13:13:03 +0000

    Initial commit

---
+++ TrilbyTV/TrilbyTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrilbyTVApp.swift
+//  TrilbyTV
+//
+//  Created by Andres Felipe Alzate Restrepo on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TrilbyTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
